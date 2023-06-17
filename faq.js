531fe1995fb3fc4c817140ad62132fcdcc8b66fb
DerQh <sanafredrick@gmail.com>
2023-06-17 16:31:19 +0300

    Js changes

--- faq.js
+++ faq.js
@@ -8,6 +8,5 @@ const btns = document.querySelectorAll(".btn");
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (event) {
     btn.parentNode.classList.toggle("active");
-    // console.log(parentNode.classList);
   });
 });
